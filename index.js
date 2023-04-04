db935c63357c6f126caf06f1979c60894da5cc6d
Jinwon Jung <113844225+zinwonzung@users.noreply.github.com>
2023-04-05 01:55:56 +0900

    섭시화씨변환
    섭시화씨변환

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Clock from './clock'
+import Calculator from './Calculator';
+
+
+  ReactDOM.render(
+      <Calculator/>,
+   document.getElementById('root')
+  );
